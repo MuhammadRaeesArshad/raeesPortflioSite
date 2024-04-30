483ce4cd1029154c28b485874e7d8c5cd57ba67e
Muhammad Raees Arshad <muhammad.raees@ezwi.fi>
2024-04-30 14:13:30 +0500

    made some projects

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="mx-5 lg:mx-[72px] mt-[30px] flex justify-between my-[35px]">
-    <h1 class="font-bold text-[32px] text-[#42446E]">MRA</h1>
-    <div class="flex">
-      <div class="flex space-x-[59px] items-center font-medium text-lg text-[#666666]">
-        <p>Home</p>
-        <p>About</p>
-        <p>Tech Stack</p>
-        <p>Projects</p>
-        <p>Contact</p>
-      </div>
-      <div class="ml-[62px] py-2 px-4 bg-[#42446E] text-[#FFFFFF] font-medium text-lg rounded-lg">
-        My Resume
+  <div class="flex justify-center items-center">
+    <div class="mx-5 lg:mx-[72px] mt-[30px] flex justify-between my-[35px] max-w-[1296px] w-full">
+      <h1 class="font-bold text-[32px] text-[#42446E]">MRA</h1>
+      <div class="flex">
+        <div class="flex space-x-[59px] items-center font-medium text-lg text-[#666666]">
+          <p>Home</p>
+          <p>About</p>
+          <p>Tech Stack</p>
+          <p>Projects</p>
+          <p>Contact</p>
+        </div>
+        <div class="ml-[62px] py-2 px-4 bg-[#42446E] text-[#FFFFFF] font-medium text-lg rounded-lg">
+          My Resume
+        </div>
       </div>
     </div>
   </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,54 +1,112 @@
 <template>
-  <div class="w-full px-5 lg:px-[72px]">
-    <!-- Title Screen -->
-    <div class="mt-[165px] flex justify-between w-full">
-      <div class="bg-[666666aa] font-bold text-[#42446E] text-[58px] w-fit">
-        <p>Hi 👋,</p>
-        <p>My name is</p>
-        <p
-          class="bg-gradient-to-r from-[#13B0F5] to-[#E70FAA] inline-block text-transparent bg-clip-text"
-        >
-          Muhammad Raees
-        </p>
-        <p class="">I build things for humans</p>
+  <div class="flex justify-center items-center">
+    <div class="w-full px-5 lg:px-[72px] max-w-[1440px]">
+      <!-- Title Screen -->
+      <div class="mt-[165px] flex justify-between w-full">
+        <div class="bg-[666666aa] font-bold text-[#42446E] text-[58px]">
+          <p>Hi 👋,</p>
+          <p>My name is</p>
+          <p
+            class="bg-gradient-to-r from-[#13B0F5] to-[#E70FAA] inline-block text-transparent bg-clip-text"
+          >
+            Muhammad Raees
+          </p>
+          <p class="">I build things for humans</p>
+        </div>
+        <img class="hidden md:block" src="../assets/TitlePageImage.svg" />
       </div>
-      <img class="" src="../assets/TitlePageImage.svg" />
-    </div>
-    <!-- My Tech Stack -->
-    <div class="mt-[370px] text-center">
-      <div id="myTechStack" class="font-bold text-[48px] text-[#42446E]">My Tech Stack</div>
-      <div class="mt-[49px] text-[32px] font-normal text-[#666666]">
-        Technologies I’ve been working with recently
+      <!-- My Tech Stack -->
+      <div class="mt-[370px] text-center">
+        <div id="myTechStack" class="font-bold text-[48px] text-[#42446E]">My Tech Stack</div>
+        <div class="mt-[49px] text-[32px] font-normal text-[#666666]">
+          Technologies I’ve been working with recently
+        </div>
+        <div class="mt-[146px] flex flex-wrap w-full">
+          <img src="../assets/JS.svg" />
+          <img src="../assets/html5.svg" />
+          <img src="../assets/React.svg" />
+          <img src="../assets/CSS.svg" />
+          <img src="../assets/tailwind.svg" />
+          <img src="../assets/CSS.svg" />
+          <img src="../assets/CSS.svg" />
+          <img src="../assets/CSS.svg" />
+          <img src="../assets/CSS.svg" />
+          <img src="../assets/CSS.svg" />
+          <img src="../assets/CSS.svg" />
+        </div>
       </div>
-      <div class="mt-[146px] grid grid-rows-2 grid-flow-col">
-        <img src="../assets/JS.svg" />
-        <img src="../assets/html5.svg" />
-        <img src="../assets/React.svg" />
-        <img src="../assets/CSS.svg" />
-        <img src="../assets/tailwind.svg" />
-        <img src="../assets/CSS.svg" />
-        <img src="../assets/CSS.svg" />
-        <img src="../assets/CSS.svg" />
-        <img src="../assets/CSS.svg" />
-        <img src="../assets/CSS.svg" />
-        <img src="../assets/CSS.svg" />
+      <!-- Projects -->
+      <div class="mt-[192px] text-center w-full">
+        <div id="myProjects" class="font-bold text-[48px] text-[#42446E]">Projects</div>
+        <div class="mt-[49px] text-[32px] font-normal text-[#666666]">Things I’ve built so far</div>
+
+        <div class="mt-[96px] flex flex-wrap justify-center w-full gap-x-[45px] gap-y-[50px]">
+          <div v-for="project in projects" :key="project.title">
+            <div class="w-[373px] h-[567px] bg-white drop-shadow-xl rounded-3xl">
+              <div class="h-[260px] bg-[#313131] flex items-center justify-center rounded-t-3xl">
+                <img src="../assets/ezWifiBlackAndWhiteLogo.svg" />
+              </div>
+              <div class="text-left mx-[30px] mt-[27px]">
+                <p class="font-medium text-[28px] text-black mt-[27px]">{{ project.title }}</p>
+                <p class="font-light text-lg text-[#666666] mt-[17px]">
+                  {{ project.description }}
+                </p>
+                <p class="font-light text-sm text-[#42446E] mt-[12px]">
+                  <span class="font-normal text-base">Tech stack :</span>{{ project.techStack }}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
-<script lang="ts">
+<script>
 export default {
   name: 'Home',
-  methods: {
-    redirectToAppleApp() {
-      window.open('https://apps.apple.com/us/app/ezwifi-mobile/id6468649775')
-    },
-    redirectToPlayStoreApp() {
-      window.open('https://play.google.com/store/apps/details?id=fi.ezwi.ezwifi')
-    },
-    redirectToLinkedin() {
-      window.open('https://www.linkedin.com/company/ezwifitech/about/')
-    },
+  setup() {
+    return {
+      projects: [
+        {
+          title: 'ezWiFi Mobile App',
+          description:
+            'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content',
+          techStack: ' HTML , JavaScript, SASS, React',
+        },
+        {
+          title: 'ezWiFi Mobile App2',
+          description:
+            'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content',
+          techStack: ' HTML , JavaScript, SASS, React',
+        },
+        {
+          title: 'ezWiFi Mobile App3',
+          description:
+            'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content',
+          techStack: ' HTML , JavaScript, SASS, React',
+        },
+        {
+          title: 'ezWiFi Mobile App4',
+          description:
+            'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content',
+          techStack: ' HTML , JavaScript, SASS, React',
+        },
+        {
+          title: 'ezWiFi Mobile App5',
+          description:
+            'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content',
+          techStack: ' HTML , JavaScript, SASS, React',
+        },
+        {
+          title: 'ezWiFi Mobile App6',
+          description:
+            'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content',
+          techStack: ' HTML , JavaScript, SASS, React',
+        },
+      ],
+    }
   },
+  methods: {},
 }
 </script>
