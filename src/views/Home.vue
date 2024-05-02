35e101c26bab597e35c0e4448d0f17ee1afc89cc
Muhammad Raees Arshad <muhammad.raees@ezwi.fi>
2024-05-02 16:39:51 +0500

    made it mobile responsive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,8 @@
   <div class="flex justify-center items-center">
     <div class="w-full px-5 lg:px-[72px] max-w-[1440px]">
       <!-- Title Screen -->
-      <div class="mt-[165px] flex justify-between w-full">
-        <div class="bg-[666666aa] font-bold text-[#42446E] text-[58px]">
+      <div class="mt-[80px] lg:mt-[165px] text-center lg:text-left lg:flex justify-between w-full">
+        <div class="bg-[666666aa] font-bold text-[#42446E] text-[24px] lg:text-[58px]">
           <p>Hi 👋,</p>
           <p>My name is</p>
           <p
@@ -13,50 +13,71 @@
           </p>
           <p class="">Full Stack Developer</p>
         </div>
-        <img class="hidden md:block" src="../assets/TitlePageImage.svg" />
+        <div class="mt-[40px] lg:mt-[0px] flex w-full lg:w-fit justify-center items-center">
+          <img
+            class="w-[250px] h-[250px] lg:w-[400px] lg:h-[400px]"
+            src="../assets/TitlePageImage.svg"
+          />
+        </div>
       </div>
       <!-- My Tech Stack -->
-      <div class="mt-[370px] text-center">
-        <div id="myTechStack" class="font-bold text-[48px] text-[#42446E]">My Tech Stack</div>
-        <div class="mt-[49px] text-[32px] font-normal text-[#666666]">
+      <div class="mt-[110px] lg:mt-[370px] text-center">
+        <div id="myTechStack" class="font-bold text-[24px] lg:text-[48px] text-[#42446E]">
+          My Tech Stack
+        </div>
+        <div class="mt-[8px] lg:mt-[49px] text-[14px] lg:text-[32px] font-normal text-[#666666]">
           Technologies I’ve been working with recently
         </div>
         <div
-          class="mt-[146px] flex flex-wrap w-full gap-x-[105px] gap-y-[67px] items-center justify-center"
+          class="mt-[32px] lg:mt-[146px] flex flex-wrap w-full gap-x-[20px] gap-y-[20px] lg:gap-x-[103px] lg:gap-y-[67px] items-center justify-center"
         >
-          <img src="../assets/JS.svg" />
-          <img src="../assets/html5.svg" />
-          <img src="../assets/React.svg" />
-          <img src="../assets/CSS.svg" />
-          <img src="../assets/tailwind.svg" />
-          <img src="../assets/CSS.svg" />
-          <img src="../assets/CSS.svg" />
-          <img src="../assets/CSS.svg" />
-          <img src="../assets/CSS.svg" />
-          <img src="../assets/CSS.svg" />
+          <img class="w-[80px] h-[80px] lg:w-[105px] lg:h-[105px]" src="../assets/JS.svg" />
+          <img class="w-[80px] h-[80px] lg:w-[105px] lg:h-[105px]" src="../assets/html5.svg" />
+          <img class="w-[80px] h-[80px] lg:w-[105px] lg:h-[105px]" src="../assets/React.svg" />
+          <img class="w-[80px] h-[80px] lg:w-[105px] lg:h-[105px]" src="../assets/CSS.svg" />
+          <img class="w-[80px] h-[80px] lg:w-[105px] lg:h-[105px]" src="../assets/tailwind.svg" />
+          <img class="w-[80px] h-[80px] lg:w-[105px] lg:h-[105px]" src="../assets/CSS.svg" />
+          <img class="w-[80px] h-[80px] lg:w-[105px] lg:h-[105px]" src="../assets/CSS.svg" />
+          <img class="w-[80px] h-[80px] lg:w-[105px] lg:h-[105px]" src="../assets/CSS.svg" />
+          <img class="w-[80px] h-[80px] lg:w-[105px] lg:h-[105px]" src="../assets/CSS.svg" />
+          <img class="w-[80px] h-[80px] lg:w-[105px] lg:h-[105px]" src="../assets/CSS.svg" />
         </div>
       </div>
       <!-- Projects -->
-      <div class="mt-[192px] mb-[225px] text-center w-full">
-        <div id="myProjects" class="font-bold text-[48px] text-[#42446E]">Projects</div>
-        <div class="mt-[49px] text-[32px] font-normal text-[#666666]">Things I’ve built so far</div>
+      <div class="mt-[100px] lg:mt-[192px] mb-[225px] text-center w-full">
+        <div id="myProjects" class="font-bold text-[24px] lg:text-[48px] text-[#42446E]">
+          Projects
+        </div>
+        <div class="mt-[8px] lg:mt-[49px] text-[14px] lg:text-[32px] font-normal text-[#666666]">
+          Things I’ve built so far
+        </div>
 
-        <div class="mt-[96px] flex flex-wrap justify-center w-full gap-x-[45px] gap-y-[50px]">
+        <div
+          class="mt-[32px] lg:mt-[96px] flex flex-wrap justify-center w-full gap-x-[45px] gap-y-[50px]"
+        >
           <div v-for="project in projects" :key="project.title">
-            <div class="w-[373px] h-[567px] bg-white drop-shadow-xl rounded-3xl">
+            <div
+              class="w-[300px] h-[500px] lg:w-[373px] lg:h-[567px] bg-white drop-shadow-xl rounded-3xl"
+            >
               <div class="h-[260px] bg-[#313131] flex items-center justify-center rounded-t-3xl">
                 <img src="../assets/ezWifiBlackAndWhiteLogo.svg" />
               </div>
-              <div class="mx-[30px] text-left mt-[27px]">
-                <p class="font-medium text-[28px] text-black mt-[27px]">{{ project.title }}</p>
-                <p class="font-light text-lg text-[#666666] mt-[17px]">
+              <div class="mx-[24.4px] lg:mx-[30px] text-left mt-[21.6px] lg:mt-[27px]">
+                <p class="font-medium text-[18px] lg:text-[28px] text-black">{{ project.title }}</p>
+                <p
+                  class="font-light text-[14px] lg:text-lg text-[#666666] mt-[13.6px] lg:mt-[17px]"
+                >
                   {{ project.description }}
                 </p>
-                <p class="font-light text-sm text-[#42446E] mt-[12px]">
-                  <span class="font-normal text-base">Tech stack :</span>{{ project.techStack }}
+                <p class="font-light text-[11px] lg:text-sm text-[#42446E] mt-[9.6px] lg:mt-[12px]">
+                  <span class="font-normal text-[12px] lg:text-base">Tech stack :</span
+                  >{{ project.techStack }}
                 </p>
               </div>
-              <div v-if="project.previewLink || project.codeLink" class="mx-[30px] mt-[21px] flex">
+              <div
+                v-if="project.previewLink || project.codeLink"
+                class="mx-[24.4px] lg:mx-[30px] mt-[18px] lg:mt-[21px] flex"
+              >
                 <div
                   v-if="project.previewLink"
                   :onclick="
@@ -66,8 +87,13 @@
                   "
                   class="flex cursor-pointer"
                 >
-                  <img src="../assets/linkChain.svg" />
-                  <p class="ml-[10px] text-black font-normal text-base underline">Live Preview</p>
+                  <img
+                    class="w-[16.5px] h-[16.5px] lg:w-[20px] lg:h-[20px]"
+                    src="../assets/linkChain.svg"
+                  />
+                  <p class="ml-[10px] text-black font-normal text-[12px] lg:text-base underline">
+                    Live Preview
+                  </p>
                 </div>
                 <div
                   v-if="project.codeLink"
@@ -79,8 +105,13 @@
                   :class="project.previewLink ? 'ml-[48px]' : ''"
                   class="flex cursor-pointer"
                 >
-                  <img src="../assets/viewCodeIcon.svg" />
-                  <p class="ml-[10px] text-black font-normal text-base underline">View Code</p>
+                  <img
+                    class="w-[16.5px] h-[16.5px] lg:w-[20px] lg:h-[20px]"
+                    src="../assets/viewCodeIcon.svg"
+                  />
+                  <p class="ml-[10px] text-black font-normal text-[12px] lg:text-base underline">
+                    View Code
+                  </p>
                 </div>
               </div>
             </div>
@@ -127,12 +158,12 @@ manager, account management system and ads manager system`,
           codeLink: 'https://github.com/MuhammadRaeesArshad/Natours---Tourism-webapp',
         },
 
-        // {
-        //   title: 'ezWiFi Mobile App6',
-        //   description:
-        //     'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content',
-        //   techStack: ' HTML , JavaScript, SASS, React',
-        // },
+        {
+          title: 'Tourism Webapp',
+          description: `Crafted a user interactive web app to allow users to view tours and book/manage their tours and accounts`,
+          techStack: ' Pug, NodeJS, ExpressJS',
+          codeLink: 'https://github.com/MuhammadRaeesArshad/Natours---Tourism-webapp',
+        },
       ],
     }
   },
