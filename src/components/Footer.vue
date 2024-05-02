ec3fdda474a229202a8b809e9dbead0e846c95c1
Muhammad Raees Arshad <muhammad.raees@ezwi.fi>
2024-05-02 16:50:32 +0500

    made it mobile responsive about and nav left

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,22 +1,27 @@
 <template lang="">
   <!-- NavBar -->
-  <div class="mx-5 lg:mx-[72px] flex justify-center items-center">
+  <div class="mx-[22px] lg:mx-[72px] flex justify-center items-center">
     <div id="contact" class="max-w-[1296px] w-full">
-      <div class="md:flex justify-between items-center">
-        <p class="bg-[666666aa] font-bold text-[#42446E] text-[32px]">MRA</p>
+      <div class="flex justify-between items-center">
+        <p class="bg-[666666aa] font-bold text-[#42446E] text-[24px] lg:text-[32px]">MRA</p>
         <div class="md:flex">
-          <p class="text-[#42446E] text-lg font-normal">+923315989606</p>
-          <p class="md:ml-[33px] text-[#42446E] text-lg font-normal">mrasoftdev@gmail.com</p>
-          <div class="md:ml-[48px] flex justify-start items-center">
+          <p class="hidden lg:block mr-[33px] text-[#42446E] text-lg font-normal">+923315989606</p>
+          <p class="hidden lg:block mr-[48px] text-[#42446E] text-lg font-normal">
+            mrasoftdev@gmail.com
+          </p>
+          <div class="flex justify-start items-center">
             <img src="../assets/Github.svg" />
             <img class="ml-[22px]" src="../assets/LinkedIn.svg" />
           </div>
         </div>
       </div>
-      <div class="mt-[43px] border-t-2 border-[#666666]/[0.3] w-full"></div>
-      <div class="mt-[43px] mb-[60px] flex justify-between">
+      <p class="lg:hidden mt-4 text-[#42446E] text-[14px] font-normal">+923315989606</p>
+      <p class="lg:hidden mt-4 text-[#42446E] text-[14px] font-normal">mrasoftdev@gmail.com</p>
+
+      <div class="mt-[14px] lg:mt-[43px] border-t-2 border-[#666666]/[0.3] w-full"></div>
+      <div class="mt-[21px] lg:mt-[43px] mb-[20px] lg:mb-[60px] flex justify-between">
         <div
-          class="hidden lg:flex flex space-x-[59px] items-center font-normal text-lg text-[#666666]"
+          class="hidden xl:flex flex space-x-[59px] items-center font-normal text-lg text-[#666666]"
         >
           <p class="cursor-pointer" :onclick="() => navigateTo('app')">Home</p>
           <p class="cursor-pointer" :onclick="() => navigateTo('about')">About</p>
@@ -24,7 +29,7 @@
           <p class="cursor-pointer" :onclick="() => navigateTo('myProjects')">Projects</p>
           <p class="cursor-pointer" :onclick="() => navigateTo('contact')">Contact</p>
         </div>
-        <p class="text-lg font-normal text-[#666666]">
+        <p class="text-[14px] text-center xl:text-left lg:text-lg font-normal text-[#666666]">
           Designed and built by
           <span
             class="bg-gradient-to-r from-[#13B0F5] to-[#E70FAA] inline-block text-transparent bg-clip-text"
